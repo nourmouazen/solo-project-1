844e472771d50206913df115cdf3271f8ec33e45
nour mouazen <nh.yu.mo@hotmail.com>
2020-10-07 11:17:25 +0300

    day2-1

--- main-1-.js
+++ main-1-.js
@@ -5,12 +5,12 @@
 
  $("#start-button").click(function(){
  	var url = ("file:///C:/Users/Toshiba/Desktop/solo-project-1/home.html");
-	   window.open(url);
+	   window.open(url,"_self");
    });
 
  // our community page //
  //it doesn't work yet
- $("#addFeedback").click(function(){
+/* $("#addFeedback").click(function(){
  	console.log("i work")
  	var feedback = [];
  	var add = $("#feedback").val()
@@ -18,25 +18,42 @@
  		alert("You must write your feedback")
  	}else{
  		$("#CommunityList").append(add)
- 		feedback.push(add)
+ 		add.map(function(){
+ 			return this.id;
+ 			.get()
+            .join();
+ 		})
  	}
- });
+ }); */
 //// BACK ///
  //back to home
  $("#home").click(function(){
  	var url =("file:///C:/Users/Toshiba/Desktop/solo-project-1/home.html");
- 	window.open(url);
+ 	window.open(url,"_self");
  })
 
  //go to about us 
   $("#about-us").click(function(){
   	var url =("file:///C:/Users/Toshiba/Desktop/solo-project-1/about-us.html");
-  	window.open(url);
+  	window.open(url,"_self");
   })
 
   //go to our-community
    $("#our-community").click(function(){
-   	console.log("iam work")
    	var url = ("file:///C:/Users/Toshiba/Desktop/solo-project-1/our-community.html");
-   	window.open(url);
+   	window.open(url,"_self");
+   })
+   //back to flower page
+    $("#Flower").click(function(){
+     var url = ("file:///C:/Users/Toshiba/Desktop/solo-project-1/flower.html")
+     window.open(url,"_self")
+    })
+    //back to Gifts & Balloons
+    $("#extra").click(function(){
+     var url = ("file:///C:/Users/Toshiba/Desktop/solo-project-1/Gifts&Balloons.html")
+     window.open(url,"_self")
+    })
+   //home button
+   $("#Flower").mouseout(function(){
+
    })
